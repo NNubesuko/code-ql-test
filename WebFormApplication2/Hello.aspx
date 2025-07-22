a5349851abc9fd2c930080f82f4205e10b921d0b
kataoka.yoshinobu <kataoka.yoshinobu@robotpayment.co.jp>
2025-07-22 12:34:54 +0900

    .NET8からFramework4.8へ入れ替え

---
+++ WebFormApplication2/Hello.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Hello.aspx.cs" Inherits="WebFormApplication2.Hello" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <h2>Hello</h2>
+    <form id="form1" runat="server">
+        <asp:Label Text="Test Input" runat="server" />
+        <asp:TextBox ID="TestInput" runat="server" />
+        <asp:Button ID="TestButton" Text="Submit" runat="server" OnClick="ClickTestButton" />
+        <asp:Label ID="Output" Text="" runat="server" />
+    </form>
+</body>
+</html>
---
+++ WebFormApplication2/Hello.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+using System.Web.UI;
+
+namespace WebFormApplication2
+{
+    public partial class Hello : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void ClickTestButton(object sender, EventArgs e)
+        {
+            var builder =
+                new SqlConnectionStringBuilder(Environment.GetEnvironmentVariable("TEST_CONNECTION"))
+                {
+                    UserID = Environment.GetEnvironmentVariable("TEST_USER_NAME"),
+                    Password = Environment.GetEnvironmentVariable("TEST_PASSWORD"),
+                };
+            var connectionString = builder.ToString();
+
+            var testDbName = ConfigurationManager.AppSettings["TEST_DB_NAME"];
+            var testSchemaName = ConfigurationManager.AppSettings["TEST_SCHEMA_NAME"];
+            var testTableName = ConfigurationManager.AppSettings["TEST_TABLE_NAME"];
+
+            var column1 = ConfigurationManager.AppSettings["TEST_COLUMN1"];
+            var column2 = ConfigurationManager.AppSettings["TEST_COLUMN2"];
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var command =
+                    new SqlCommand(
+                        $"SELECT TOP 100 * FROM [{testDbName}].[{testSchemaName}].[{testTableName}] WHERE [{column1}] = {TestInput.Text} ORDER BY [{column2}] DESC",
+                        connection);
+
+                var sb = new StringBuilder();
+                using (var reader = command.ExecuteReader())
+                {
+                    var index = 1;
+                    while (reader.Read())
+                    {
+                        sb.Append($"{index++}, Column1: {reader[column1]}, Column2: {reader[column2]}")
+                            .Append("<br>");
+                    }
+                }
+
+                var result = sb.ToString();
+                Output.Text = string.IsNullOrEmpty(result) ? "データ無し" : result;
+            }
+        }
+    }
+}
